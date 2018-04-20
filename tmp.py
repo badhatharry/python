a60e89086c1d86b2e681590231e0ec70a5e9b66c
Jan Röder <jroeder@users.fias.science>
2018-04-20 13:29:13 +0200

    workes all well so far

--- tmp.py
+++ tmp.py
@@ -1,4 +1,19 @@
 import matplotlib.pyplot as plt
-plt.plot([1,2,3,4])
-plt.ylabel('some numbers')
+import numpy as np
+import csv
+
+x = []
+y = []
+
+with open('kalorimeter.txt', 'r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        x.append(float(row[0]))
+        y.append(float(row[1]))
+
+plt.plot(x,y,'bo' ,label='Heizkurve')
+
+plt.title('Kalorimeterkurve\n400g Wasser')
+plt.ylabel('Temperatur T/K')
+plt.xlabel('Zeit t/s')
 plt.show()
