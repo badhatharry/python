c9b51eaee110e828780c435f6b4c8235d9bbf187
Jan Röder <jroeder@users.fias.science>
2018-04-20 14:07:58 +0200

    meh

--- tmp.py
+++ tmp.py
@@ -1,17 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+from numpy import *
+from scipy.interpolate import *
 
 x = []
 y = []
 
-with open('kalorimeter.txt', 'r') as csvfile:
+with open('kalo_cropped.txt', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x.append(float(row[0]))
         y.append(float(row[1]))
 
-plt.plot(x,y,'bo' ,label='Heizkurve')
+p1 = polyfit(x, y, 1)
+print(p1)
+
+plt.plot(x, y,'o' ,label='Heizkurve')
+plt.plot(x, polyval(p1,x))
 
 plt.title('Kalorimeterkurve\n400g Wasser')
 plt.ylabel('Temperatur T/K')
