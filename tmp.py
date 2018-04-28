1b2105caca4b4f66d8120950d041ca7bb9782e52
Jan <34909681+badhatharry@users.noreply.github.com>
2018-04-28 09:54:44 +0200

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -13,11 +13,11 @@ with open('kalo_cropped.txt', 'r') as csvfile:
         x.append(float(row[0]))
         y.append(float(row[1]))
 
-p1 = polyfit(x, y, 1)
-print(p1)
+#p1 = polyfit(x, y, 1)
+#print(p1)
 
 plt.plot(x, y,'o' ,label='Heizkurve')
-plt.plot(x, polyval(p1,x))
+#plt.plot(x, polyval(p1,x))
 
 plt.title('Kalorimeterkurve\n400g Wasser')
 plt.ylabel('Temperatur T/K')
